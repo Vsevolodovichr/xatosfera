47ff749140e3f8245d7b5b2a6eaa902ffe860fff
Roman Shkurenko <130462736+Vsevolodovichr@users.noreply.github.com>
2026-02-22 22:58:59 +0000

    2222

--- supabase/migrations/20260202040646_196dd563-3f88-4e6f-ad8a-1f01821111e1.sql
+++
@@ -1,5 +0,0 @@
--- Allow top-managers to update (approve) manager profiles
-CREATE POLICY "Top managers can update manager profiles" 
-ON public.profiles 
-FOR UPDATE 
-USING (has_role(auth.uid(), 'top_manager'::app_role) AND (get_user_role(id) = 'manager'::app_role));
--- supabase/migrations/20260204003334_6e597bb6-19a0-4522-b701-de199352cb5f.sql
+++
@@ -1,7 +0,0 @@
--- Add assigned_manager_id column for responsible manager assignment
-ALTER TABLE public.properties 
-ADD COLUMN IF NOT EXISTS assigned_manager_id uuid REFERENCES public.profiles(id);
-
--- Create index for faster lookups
-CREATE INDEX IF NOT EXISTS idx_properties_assigned_manager ON public.properties(assigned_manager_id);
-CREATE INDEX IF NOT EXISTS idx_properties_user_id ON public.properties(user_id);
